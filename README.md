2fd764e278d0e96aa68b46d6d2d066887f916c96
albert patterson <apatterson189@gmail.com>
2022-10-30 12:09:04 -0500

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Create a printer friendly version of a README.md
+
+In some cases likes workshops, classrooms, etc it can be helpful to have a printout of the readme. However, tyipcal formatting of items like URLs would make printouts of typical README files less useful. This tool will add information to the README to make it more useful as a printout, though less asthetically pleasing overall.
+
+## Usage
+
+- CD to the directory with the desired README.md
+- `npx make-printer-friendly-readme1`
+
+A new file named README_PRINTABLE.md will be created
---
+++ index.js
@@ -0,0 +1,46 @@
+import path from 'path';
+import fs from 'fs';
+import process from 'process';
+const readmePath = path.resolve(process.cwd(), 'README.md');
+const writePath = path.resolve(process.cwd(), 'README_PRINTER_FRIENDLY.md');
+
+(async () => {
+  await fs.promises.access(readmePath, fs.constants.F_OK);
+
+  const markdown = await fs.promises.readFile(readmePath);
+
+  const transformed = makePrinterFrieldly(markdown.toString());
+
+  await fs.promises.writeFile(writePath, transformed);
+})();
+
+function makePrinterFrieldly(markdown) {
+  let printerFriendly = makeLinksPrinterFriendly(markdown);
+
+  return printerFriendly;
+}
+
+const LINK_RE = /[^!]\[[^\]]*\]\(([^\)]*)\)/g;
+function makeLinksPrinterFriendly(markdown) {
+  let parts = [];
+  let prevEnd = 0;
+
+  let match;
+  while ((match = LINK_RE.exec(markdown))) {
+    if (!match) {
+      break;
+    }
+
+    const matchText = match[0];
+    const matchURL = match[1];
+    const index = match.index;
+    const matchEnd = index + matchText.length;
+    parts.push(markdown.slice(prevEnd, matchEnd));
+    prevEnd = matchEnd;
+    parts.push(`: ${matchURL}`);
+  }
+
+  parts.push(markdown.slice(prevEnd, markdown.length));
+
+  return parts.join('');
+}
